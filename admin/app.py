5fc3520ffb924f6df965c5cf15b08f52d8c83358
jessymgu <katemgu@gmail.com>
2021-10-29 23:47:15 -0500

    Conexión superadmin+BD. Modificación html superadm

--- admin/app.py
+++ admin/app.py
@@ -20,26 +20,68 @@ def home():
 
 @app.route('/superadmin-dashboard', methods=["GET"])
 def superadmin_index():
-    return render_template('index.html')
+    # return render_template('index.html')
     # !El dashboard únicamente muestra la información consignada en la BD. Desde el navegador no se envía información al servidor. Por lo tanto, usa exclusivamente el método "GET"
+    try:
+        with sqlite3.connect('DB_Clinica_RC.db') as con:
+            con.row_factory =sqlite3.Row #convierte la respuesta de la Bd en un diccionario
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Usuarios")
+            row = cur.fetchall()
+            return render_template('index.html',row=row)
+    except Error:
+        print(Error)
+
 
 @app.route('/superadmin-pacientes', methods=["GET", "POST"])
 def superadmin_pacientes():
-    return render_template('patients.html')
+    # return render_template('patients.html')
     # !Primero se usa el método GET para consultar a la información alojada en la BD (vista de la página pacientes)
     # !Luego, el administrador, si así lo decide, usando el método POST, envía información a la BD. Puesto que el superadministrador puede crear (registrar) un nuevo paciente, editar, o eliminar su información. Para ello, envía la información desde el navegador hasta el servidor (crear, editar o eliminar)
+    try:
+        with sqlite3.connect('DB_Clinica_RC.db') as con:
+            con.row_factory =sqlite3.Row #convierte la respuesta de la Bd en un diccionario
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Usuarios")
+            # cur.execute("SELECT * FROM Pacientes")
+            row = cur.fetchall()
+            return render_template('patients.html',row=row)
+    except Error:
+        print(Error)
+
 
 @app.route('/superadmin-medicos', methods=["GET", "POST"])
 def superadmin_medicos():
-    return render_template('doctors.html')
+    # return render_template('doctors.html')
     # !Primero se usa el método GET para consultar a la información alojada en la BD (vista de la página medicos)
     # !Luego, el administrador, si así lo decide, usando el método POST, envía información a la BD. Puesto que el superadministrador puede crear (registrar) un nuevo médico, editar, o eliminar su información. Para ello, envía la información desde el navegador hasta el servidor (crear, editar o eliminar)
+    try:
+        with sqlite3.connect('DB_Clinica_RC.db') as con:
+            con.row_factory =sqlite3.Row #convierte la respuesta de la Bd en un diccionario
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Usuarios")
+            # cur.execute("SELECT * FROM Medicos")
+            row = cur.fetchall()
+            return render_template('doctors.html',row=row)
+    except Error:
+        print(Error)
+
 
 @app.route('/superadmin-citas', methods=["GET", "POST"])
 def superadmin_citas():
-    return render_template('appointments.html')
+#     return render_template('appointments.html')
     # !Primero se usa el método GET para consultar a la información alojada en la BD (vista de la página citas)
     # !Luego, el administrador, si así lo decide, usando el método POST, envía información a la BD. Puesto que el superadministrador puede crear una nueva cita, editar, o eliminar su información. Para ello, envía la información desde el navegador hasta el servidor (crear, editar o eliminar)
+    try:
+        with sqlite3.connect('DB_Clinica_RC.db') as con:
+            con.row_factory =sqlite3.Row #convierte la respuesta de la Bd en un diccionario
+            cur = con.cursor()
+            cur.execute("SELECT * FROM citaspaciente")
+            row = cur.fetchall()
+            return render_template('appointments.html',row=row)
+    except Error:
+        print(Error)
+
 
 @app.route('/route_name')
 def method_name():
